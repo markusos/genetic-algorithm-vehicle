ca102889b229c66742cb39e2f8e8ce9731892d23
Anders.Sandstrom89@gmail.com <Anders.Sandstrom89@gmail.com@60df3d32-7b68-f15d-3acf-9e4f7cec24d8>
2011-03-27 16:42:39 +0000

    fixed the compilation bug in Config

--- trunk/src/config.h
+++ trunk/src/config.h
@@ -7,51 +7,11 @@ namespace GA_VEHICLE
 {
 	class Config
 	{
-	private:
-		static Config* instance;
-		Config()
-		{
-			mutationChance = 3;
-			mutationFactor = 1.0;
-
-			tournamentSize = 2;
-			toCrossOverSize = 10;
-
-			populationSize = 20;
-
-			verticeMinLength = 0.5; 
-			verticeMaxLength = 10;
-			verticeCount = 8;
-
-			wheelMinSize = 1; 
-			wheelMaxSize = 3;
-
-			velocityIterations = 10;
-			positionIterations = 10;
-
-			wheelTorqueMin = 50;
-			wheelTorqueMax = 1000;
-
-			wheelSpeedMin = 2;
-			wheelSpeedMax = 10;
-
-			allowedStandStillSteps = 500; // 60/sec
-			minMove = 5;
-		}
+	
 	public:
-		
-		static Config* get()
-		{
-			if(!instance)
-			{
-				instance = new Config();
-			}
-			return instance;
-		}
-		void LoadFromFile(std::string filename)
-		{
+		static Config* get();
+		void LoadFromFile(std::string filename);
 
-		}
 		int mutationChance; // %
 		float mutationFactor;
 
@@ -78,7 +38,8 @@ namespace GA_VEHICLE
 		long allowedStandStillSteps; // 60/sec
 		float minMove;
 		
-	
+	private:
+		Config();
 	};
 }
 
